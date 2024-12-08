cb21f72318c819ee10f43ea633f4dc32142568b0
Balychev Kiril <mister.balychev@gmail.com>
2024-12-08 11:25:50 +0200

    Добавьте файлы проекта.

---
+++ AsynchronousPractice/Controllers/UserInfoSyncController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AsynchronousPractice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserInfoSyncController : ControllerBase
+    {
+        private const string USERS_FILE_PATH = "Data/users.json";
+        private const string LOCATIONS_FILE_PATH = "Data/location.json";
+        private const string GAMES_FILE_PATH = "Data/games.json";
+
+        [HttpGet]
+        public ActionResult GetUserInfo()
+        {
+          
+        }
+
+        private string GetRandomUserIdAsync()
+        {
+
+        }
+        private string GetUserLocationAsync(int userId)
+        {
+
+        }
+
+        private string GetUserFavoriteGameAsync(int userId)
+        {
+
+        }
+    }
+}
