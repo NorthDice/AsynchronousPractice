ab1119b6b0f459150a7284b15e6cf14bfdc71222
Balychev Kiril <mister.balychev@gmail.com>
2024-12-08 12:10:06 +0200

    Implemented SyncControler

--- AsynchronousPractice/Controllers/UserInfoSyncController.cs
+++ AsynchronousPractice/Controllers/UserInfoSyncController.cs
@@ -1,37 +1,51 @@
 ﻿using AsynchronousPractice.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using System.Text.Json;
 
 namespace AsynchronousPractice.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class UserInfoSyncController : ControllerBase
+    [Route("[controller]")]
+    public class UserInfoSyncController : Controller
     {
         private const string USERS_FILE_PATH = "Data/users.json";
         private const string LOCATIONS_FILE_PATH = "Data/location.json";
         private const string GAMES_FILE_PATH = "Data/games.json";
 
+
         [HttpGet]
+        [Route("GetUserInfo")]
         public ActionResult GetUserInfo()
         {
-            var userId = GetRandomUserId();
+            try
+            {
+                var userId = GetRandomUserId();
+                var location = GetUserLocation(userId);
+                var favoriteGame = GetUserFavoriteGame(userId);
 
-            var location = GetUserLocation(userId);
+                return Ok(new { userId, location, favoriteGame });
+            }
+            catch (NullReferenceException ex)
+            {
+                Debug.WriteLine($"Error occurred: {ex.Message}");
 
-            var favoriteGame = GetUserFavoriteGame(userId);
-
-            return Ok(new { userId, location, favoriteGame });
+                return StatusCode(500, new
+                {
+                    Error = "An error occurred while fetching user info",
+                    Details = ex.Message
+                });
+            }
 
         }
 
         private int GetRandomUserId()
         {
-            Console.WriteLine("Reading users file");
+            Debug.WriteLine("Reading users file");
             var userJson = System.IO.File.ReadAllText(USERS_FILE_PATH);
             Thread.Sleep(1000);
 
-            Console.WriteLine("Users file readed successfully");
+            Debug.WriteLine("Users file readed successfully");
 
             var userData = JsonSerializer.Deserialize<UserData>(userJson) 
                 ?? throw new NullReferenceException("Users json is empty!");
@@ -41,11 +55,11 @@ namespace AsynchronousPractice.Controllers
 
         private string GetUserLocation(int userId)
         {
-            Console.WriteLine("Reading location file");
+            Debug.WriteLine("Reading location file");
             var locationJson = System.IO.File.ReadAllText(LOCATIONS_FILE_PATH);
             Thread.Sleep(3000);
 
-            Console.WriteLine("Location file readed successfully");
+            Debug.WriteLine("Location file readed successfully");
 
             var locationData = JsonSerializer.Deserialize<LocationData>(locationJson)
                 ?? throw new NullReferenceException("Location json is empty!");
@@ -56,11 +70,11 @@ namespace AsynchronousPractice.Controllers
 
         private string GetUserFavoriteGame(int userId)
         {
-            Console.WriteLine("Reading games file");
+            Debug.WriteLine("Reading games file");
             var gameJson = System.IO.File.ReadAllText(GAMES_FILE_PATH);
             Thread.Sleep(3000);
 
-            Console.WriteLine("Games file readed successfully");
+            Debug.WriteLine("Games file readed successfully");
 
             var gameData = JsonSerializer.Deserialize<GameData>(gameJson)
                 ?? throw new NullReferenceException("Game json is empty!");
